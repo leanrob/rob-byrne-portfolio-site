66819e651feff4f9cc7cfcfd596d40c48088f28a
Rob Byrne <rob@leanrob.com>
2019-04-22 22:12:24 -0700

    Heading section as an intro to the site
    - Site intro section for the portfolio site
    - Made with CSS shapes and rotations
    - Image fading in CSS shapes
    - New images added for the front of the site
    - No menu made yet, should add internal menu

---
+++ src/components/HeaderSection/HeaderSection.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="shape_contain">
+    <div id="circle"></div>
+    <div id="tri1"></div>
+    <div id="strip1" class="strip"></div>
+    <div id="tri2"></div>
+    <div id="tri3"></div>
+    <div id="strip2" class="strip"></div>
+    <div id="strip3" class="strip"></div>
+    <div id="leftside">Build > Test > Measure > Iterate</div>
+    <div id="rightside">Rust > React > Vue > GCP</div>
+    <div id="topLeft">Entrepreneur
+      <br><span>Software Dev</span></div>
+    <div id="topRight">2019</div>
+    <div id="headline">Rob Byrne</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderSection",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HeaderSection msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderSection from "../components/HeaderSection/HeaderSection";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+	  HeaderSection,
   }
 };
 </script>
