62637e48f8fa55623f78a536e3e0ea1c597a3e27
Rob Byrne <rob@leanrob.com>
2019-04-28 02:52:13 -0700

    Add languages frameworks section
    - add photos for the section
    - add files and import scss file for new section

---
+++ src/components/Home/LanguagesFrameworksSection/LanguagesFrameworksSection.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="languages-frameworks-section">
+        
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "LanguagesFrameworksSection"
+	}
+</script>
+
+<style scoped>
+
+</style>
