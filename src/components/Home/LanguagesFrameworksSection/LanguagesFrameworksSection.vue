ac48cb4153c9407f9911f80431b18b11ec406bfb
Rob Byrne <rob@leanrob.com>
2019-05-01 00:26:00 -0700

    Adding alt elements to images

--- src/components/Home/LanguagesFrameworksSection/LanguagesFrameworksSection.vue
+++ src/components/Home/LanguagesFrameworksSection/LanguagesFrameworksSection.vue
@@ -7,75 +7,75 @@
         </div>
         <div class="languages-frameworks-section__logos">
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/javascript-logo.png"/>
+                <img src="./../../../assets/logos/javascript-logo.png" alt="Javascript"/>
                 <p>
                     Javascript
                 </p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/vue-logo.png"/>
+                <img src="./../../../assets/logos/vue-logo.png" alt="Vue"/>
                 <p>
                     Vue
                 </p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/react-logo.png"/>
+                <img src="./../../../assets/logos/react-logo.png" alt="React"/>
                 <p>
                     React
                 </p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/redux-logo.png"/>
+                <img src="./../../../assets/logos/redux-logo.png" alt="Redux"/>
                 <p>
                     Redux
                 </p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/kubernetes-logo.png"/>
+                <img src="./../../../assets/logos/kubernetes-logo.png" alt="Kubernetes"/>
                 <p>
                     Kubernetes
                 </p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/gcp-logo.png"/>
+                <img src="./../../../assets/logos/gcp-logo.png" alt="Google Cloud Platform"/>
                 <p>Google Cloud</p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/postgres-logo.png"/>
+                <img src="./../../../assets/logos/postgres-logo.png" alt="Postgres"/>
                 <p>Postgres</p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/html-logo.png"/>
+                <img src="./../../../assets/logos/html-logo.png" alt="HTML5"/>
                 <p>
                     Html
                 </p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/sass-logo.png"/>
+                <img src="./../../../assets/logos/sass-logo.png" alt="Sass"/>
                 <p>
                     Sass
                 </p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/atlassium-logo.png" />
+                <img src="./../../../assets/logos/atlassium-logo.png" alt="Jira" />
                 <p>
                     JIRA
                 </p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/nodejs-logo.png"/>
+                <img src="./../../../assets/logos/nodejs-logo.png" alt="NodeJS"/>
                 <p>
                     NodeJS
                 </p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/rust-logo.png"/>
+                <img src="./../../../assets/logos/rust-logo.png" alt="Rust"/>
                 <p>
                     Rust
                 </p>
             </div>
             <div class="languages-frameworks-section__logos--logo">
-                <img src="./../../../assets/logos/sketch-logo.png"/>
+                <img src="./../../../assets/logos/sketch-logo.png" alt="Sketch"/>
                 <p>Design</p>
             </div>
         </div>
